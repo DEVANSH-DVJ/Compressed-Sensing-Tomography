3da18bd76974722a2f79ee35aa13b2747ac52365
devansh-dvj <devansh.dvj@gmail.com>
2021-05-27 21:38:02 +0530

    Updated RU - added indentation

--- @RU/ctranspose.m
+++ @RU/ctranspose.m
@@ -1,4 +1,7 @@
+
 function  res = ctranspose(A)
 
-A.adjoint = xor(A.adjoint,1);
-res = A;
+    A.adjoint = xor(A.adjoint, 1);
+    res = A;
+
+end
--- @RU/mtimes.m
+++ @RU/mtimes.m
@@ -1,7 +1,12 @@
+
 function res = mtimes(A,x)
 
-if A.adjoint == 0 % A*x
-    res = reshape(radon(idct2(reshape(x, [A.N A.N])), A.angles), [A.M*A.Q 1]);
-else % At*x
-    res = reshape(dct2(iradon(reshape(x, [A.M A.Q]), A.angles, A.N)), [A.N*A.N 1]);
+    if A.adjoint == 0
+        % A*x
+        res = reshape(radon(idct2(reshape(x, [A.N A.N])), A.angles), [A.M*A.Q 1]);
+    else
+        % At*x
+        res = reshape(dct2(iradon(reshape(x, [A.M A.Q]), A.angles, A.N)), [A.N*A.N 1]);
+    end
+
 end
