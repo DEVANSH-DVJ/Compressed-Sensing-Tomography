759b7761801f3695cbde2c4664c04ee0835e6fb5
devansh-dvj <devansh.dvj@gmail.com>
2021-05-29 16:04:59 +0530

    Replaced RU with CS

--- @CCS2/CCS2.m
+++ @CCS2/CCS2.m
@@ -13,14 +13,14 @@ function res = CCS2(N, M, angles1, angles2)
     %   CCS2.adjoint  : 1 if transpose else 0
     %   CCS2.N2       : N * N
     %   CCS2.MQ       : M * size(angles1, 2)
-    %   CCS2.R1U      : Object of RU class for slice 1
-    %   CCS2.R2U      : Object of RU class for slice 2
+    %   CCS2.R1U      : Object of CS class for slice 1
+    %   CCS2.R2U      : Object of CS class for slice 2
 
     res.adjoint = 0;
     res.N2 = N * N;
     res.MQ = M * size(angles1, 2);
-    res.R1U = RU(N, M, angles1);
-    res.R2U = RU(N, M, angles2);
+    res.R1U = CS(N, M, angles1);
+    res.R2U = CS(N, M, angles2);
 
     % Register this variable as a CCS2 class
     res = class(res, 'CCS2');
--- @CCS3/CCS3.m
+++ @CCS3/CCS3.m
@@ -14,16 +14,16 @@ function res = CCS3(N, M, angles1, angles2, angles3)
     %   CCS3.adjoint  : 1 if transpose else 0
     %   CCS3.N2       : N * N
     %   CCS3.MQ       : M * size(angles1, 2)
-    %   CCS3.R1U      : Object of RU class for slice 1
-    %   CCS3.R2U      : Object of RU class for slice 2
-    %   CCS3.R3U      : Object of RU class for slice 3
+    %   CCS3.R1U      : Object of CS class for slice 1
+    %   CCS3.R2U      : Object of CS class for slice 2
+    %   CCS3.R3U      : Object of CS class for slice 3
 
     res.adjoint = 0;
     res.N2 = N * N;
     res.MQ = M * size(angles1, 2);
-    res.R1U = RU(N, M, angles1);
-    res.R2U = RU(N, M, angles2);
-    res.R3U = RU(N, M, angles3);
+    res.R1U = CS(N, M, angles1);
+    res.R2U = CS(N, M, angles2);
+    res.R3U = CS(N, M, angles3);
 
     % Register this variable as a CCS3 class
     res = class(res, 'CCS3');
